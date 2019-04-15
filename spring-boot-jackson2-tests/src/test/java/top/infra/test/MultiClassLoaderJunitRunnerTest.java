645baf24c32bbacbd35179422468b583079eed8c
haolun zhang <opensource@infra.top>
2019-04-16 00:45:52 +0800

    update ci config

--- spring-boot-jackson2-tests/src/test/java/top/infra/test/MultiClassLoaderJunitRunnerTest.java
+++ spring-boot-jackson2-tests/src/test/java/top/infra/test/MultiClassLoaderJunitRunnerTest.java
@@ -2,16 +2,22 @@ package top.infra.test;
 
 import static org.junit.Assert.assertEquals;
 
-//@LibrarySets(
-//    librarySets = {
-//        "http://repo1.maven.org/maven2/com/google/guava/guava/18.0/guava-18.0.jar,file:target/classes,file:target/test-classes",
-//        "http://repo1.maven.org/maven2/com/google/guava/guava/19.0/guava-19.0.jar,file:target/classes,file:target/test-classes",
-//    }
-//)
-//@RunWith(MultiClassLoaderJunitRunner.class)
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import top.infra.test.classloader.multi.LibrarySets;
+import top.infra.test.classloader.multi.MultiClassLoaderJunitRunner;
+
+@LibrarySets(
+    librarySets = {
+        "https://repo1.maven.org/maven2/com/google/guava/guava/18.0/guava-18.0.jar,file:target/classes,file:target/test-classes",
+        "https://repo1.maven.org/maven2/com/google/guava/guava/19.0/guava-19.0.jar,file:target/classes,file:target/test-classes",
+    }
+)
+@RunWith(MultiClassLoaderJunitRunner.class)
 public class MultiClassLoaderJunitRunnerTest {
 
-    //@Test
+    @Test
     public void testGuavaAndCommonUser() {
         assertEquals("AAAA", GuavaAndCommonUser.toUpperCase("AAaa"));
     }
